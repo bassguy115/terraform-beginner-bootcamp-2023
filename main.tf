7f09c5f804f786fde109b896ee5a9854511a37f3
Matt Luger <bassguy115@gmail.com>
2023-10-02 02:50:15 +0000

    Added TF cloud workspace and token for TF Cloud login

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "Bassguy115"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }  
   required_providers {
     random = {
       source = "hashicorp/random"

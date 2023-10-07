6a1dc675b968b9617fa6b049f9b2da9acc37ee04
Matt-into-Tech <145221609+bassguy115@users.noreply.github.com>
2023-10-06 20:30:57 -0400

    #28 enabled static website hosting, uploaded index and error file via terraform (#29)

--- main.tf
+++ main.tf
@@ -20,6 +20,8 @@ module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
+  error_html_filepath = var.error_html_filepath
+  index_html_filepath = var.index_html_filepath
 } 
 
 
